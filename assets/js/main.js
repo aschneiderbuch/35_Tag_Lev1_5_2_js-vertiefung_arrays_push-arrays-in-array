f579506d720a6e2583b009b3299274559d354db9
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-31 12:25:56 +0100

    testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
 console.log("test");
+/* 
+Frage
+
+Array in Array
+    heroUndEnemy []
+          
+        */
+
+
+const heroUndEnemy = [["Batman0", "The Joker0" ],["ProfessorX0", "Magneto0"],["Thor0", "Loki0"]];
+
+
+const superH1 =["Batman"];
+heroUndEnemy.push(superH1);
+superH1.push("The Joker");
+
+heroUndEnemy.push(["Supermann", "Lex Luthor"],["Wolverine", "Sabretooth"],["Ghost Rider", "Mephisto"])
+
+
+
+
+console.log(heroUndEnemy);
+console.table(heroUndEnemy);
